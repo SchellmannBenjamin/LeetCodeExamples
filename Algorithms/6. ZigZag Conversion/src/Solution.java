bd31a016d1a2fe407c29c315b26b005b4594896a
Benjamin Schellmann <43296230+SchellmannBenjamin@users.noreply.github.com>
2020-04-12 01:29:19 +0200

    fehler bei zigzag

--- Algorithms/6. ZigZag Conversion/src/Solution.java
+++ Algorithms/6. ZigZag Conversion/src/Solution.java
@@ -1,21 +1,44 @@
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(convert("PAYPALISHIRING", 3));
+        System.out.println(convert("PAYPALISHIRING", 4));
     }
     public static String convert(String s, int numRows) {
         char[] input = s.toCharArray();
-        char[][] zigzack = new char[s.length()/numRows][numRows];
-        int tmp = 0;
+        char[][] zigzack = new char[s.length()][numRows];
+        int yaxis = 0;
+        int xaxis = 0;
+        String ret = "";
+        boolean flag = true;
+        boolean done = false;
 
         for (int i = 0; i < input.length ; i++) {
-            if(tmp<numRows)zigzack[tmp/numRows][tmp]=input[i];
-            if(tmp==numRows) {
-                zigzack[][]=input[i];
+            done = false;
+            if(yaxis==0)flag=true;
+            if(yaxis<numRows&&flag&&!done){
+                zigzack[xaxis][yaxis]=input[i];
+                yaxis++;
+                done=true;
+            }
+            if(yaxis==numRows||flag==false&&!done) {
+                if(yaxis==numRows)yaxis--;
+                yaxis--;
+                xaxis++;
+                flag=false;
+                zigzack[xaxis][yaxis]=input[i];
+                done=true;
             }
 
-            tmp++;
         }
-        return "";
+        int lmao = 0;
+        for (int j = 0; j < numRows; j++) {
+            for (int i = 0; i < s.length(); i++) {
+                if (zigzack[i][j] != '\u0000') {
+                    ret += zigzack[i][j];
+                }
+            }
+        }
+
+        return ret;
     }
 
 }
