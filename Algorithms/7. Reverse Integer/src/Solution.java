ae9c5f2a2474b3308e5434c7a86bf61b4823d748
Benjamin Schellmann <43296230+SchellmannBenjamin@users.noreply.github.com>
2020-07-20 11:34:23 +0200

    -

---
+++ Algorithms/7. Reverse Integer/src/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(reverse(817));
+    }
+
+    public static int reverse(int num){
+        Integer.
+    }
+}
